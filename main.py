a263201e8aa2973971432b873fdbca5646923017
TrentMonnin7 <trentmonnin@gmail.com>
2022-07-17 14:39:48 -0400

    test python code for fun

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World")
+def count_bits(n):
+    X = bin(n)
+    count = 0
+    for i in X:
+        if i == "1":
+            count+=1
+        else:
+            pass
+    print(count)
+    return count
+
+count_bits(5)
+
